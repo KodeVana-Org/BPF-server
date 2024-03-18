e37739796f38b1f1d619dfbd3985b0bf47d42ec7
usenamedinesh <borod9200@gmail.com>
2024-03-19 03:25:50 +0530

    test3

--- src/profile/auth.js
+++ src/profile/auth.js
@@ -1,17 +1,19 @@
+const DistrictMember = require("../models/distMember.Model");
 const User = require("../models/user.Model");
 
 exports.auth = async (req, res) => {
   try {
     const userId = req.user.id;
     const user = await User.findById(userId);
-  
+    const Memeber = await DistrictMember.findOne({ user: userId });
+
     if (!user || !userId) {
       return res.status(403).json({
         success: false,
         message: "User is not exist or Invalid token",
       });
     }
-  
+
     return res.status(200).json({
       success: true,
       data: {
@@ -20,13 +22,16 @@ exports.auth = async (req, res) => {
         userId: user.userID,
         name: user.firstName + user.lastName,
         userType: user.userType,
+        id: user._id,
+        profileImage: user.profileImage ? user.profileImage : null,
+        member: Memeber ? Memeber : null,
       },
     });
   } catch (error) {
     return res.status(500).json({
       success: false,
-      message:'Failed to verify token',
-      error:error.message
-    })
+      message: "Failed to verify token",
+      error: error.message,
+    });
   }
 };
