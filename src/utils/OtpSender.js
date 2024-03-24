cd1f66e29b5674cf6d98605fd4e8583faa6e8b97
usenamedinesh <borod9200@gmail.com>
2024-03-24 13:47:29 +0530

    otp send success with nexmo

--- src/utils/OtpSender.js
+++ src/utils/OtpSender.js
@@ -31,51 +31,51 @@ function sendOTPByEmail(email, otp, htmlTemplate) {
 
 // Send OTP to a phone number
 
-// function sendOTPViaSMS(phoneNumber, otp) {
-//   // Initialize Nexmo client with your API credentials
-//   const nexmo = new Nexmo({
-//     apiKey: process.env.NEXMO_API_KEY,
-//     apiSecret: process.env.NEXMO_API_SECRET,
-//   });
-//
-//   const from = "Vonage APIs"; // Choose a sender ID provided by Nexmo
-//   const to = phoneNumber;
-//   const text = `Your OTP is: ${otp}`;
-//
-//   console.log("OTP sent to:", phoneNumber);
-//   console.log("OTP:", otp);
-//   console.log("Details:", from, to, text);
-//   nexmo.message.sendSms(from, to, text, (err, responseData) => {
-//     if (err) {
-//       console.error("Error sending OTP:", err);
-//     } else {
-//       console.log("OTP sent successfully:", responseData);
-//     }
-//   });
-// }
-
-const accountSid = process.env.ACCOUNT_SID;
-const authToken = process.env.AUTH_TOKEN;
-const twilioPhoneNumber = process.env.PHONE_NO;
-
-const client = new twilio(accountSid, authToken);
-
 function sendOTPViaSMS(phoneNumber, otp) {
+  // Initialize Nexmo client with your API credentials
+  const nexmo = new Nexmo({
+    apiKey: process.env.NEXMO_API_KEY,
+    apiSecret: process.env.NEXMO_API_SECRET,
+  });
+
+  const from = "FROM BPF"; // Choose a sender ID provided by Nexmo
   const to = phoneNumber;
-  const message = `Your OTP is: ${otp}`;
+  const text = `Your OTP is: ${otp}`;
 
   console.log("OTP sent to:", phoneNumber);
   console.log("OTP:", otp);
-  console.log("Message:", message);
-
-  client.messages
-    .create({
-      body: message,
-      from: twilioPhoneNumber,
-      to: to,
-    })
-    .then((message) => console.log("OTP sent successfully:", message.sid))
-    .catch((error) => console.error("Error sending OTP:", error));
+  console.log("Details:", from, to, text);
+  nexmo.message.sendSms(from, to, text, (err, responseData) => {
+    if (err) {
+      console.error("Error sending OTP:", err);
+    } else {
+      console.log("OTP sent successfully:", responseData);
+    }
+  });
 }
+
+// const accountSid = process.env.ACCOUNT_SID;
+// const authToken = process.env.AUTH_TOKEN;
+// const twilioPhoneNumber = process.env.PHONE_NO;
+//
+// const client = new twilio(accountSid, authToken);
+//
+// function sendOTPViaSMS(phoneNumber, otp) {
+//   const to = phoneNumber;
+//   const message = `Your OTP is: ${otp}`;
+//
+//   console.log("OTP sent to:", phoneNumber);
+//   console.log("OTP:", otp);
+//   console.log("Message:", message);
+//
+//   client.messages
+//     .create({
+//       body: message,
+//       from: twilioPhoneNumber,
+//       to: to,
+//     })
+//     .then((message) => console.log("OTP sent successfully:", message.sid))
+//     .catch((error) => console.error("Error sending OTP:", error));
+// }
 
 module.exports = { sendOTPByEmail, sendOTPViaSMS };
